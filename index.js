9e79581e0da685ecde2d028a294c5eba2b73880e
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-12-04 11:35:16 -0700

    working on config for db

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const cors = require('cors');
+
+const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const fetch = require("node-fetch");
+const knex = require("knex");
+const config = require("./config/config.js");
+
+//won't break if .env is not present
+require("dotenv").config();
+
+function DBEnvironment() {
+    if (process.env.NODE_ENV === "development") {
+        return (
+            knex({
+                client: "pg",
+                connection: {
+                    host: "127.0.0.1",
+                    user: config.DBUser,
+                    password: config.DBPassword,
+                    database: config.DBLocal
+                }
+            })
+        )
+    }
+    if (process.env.NODE_ENV === "production") {
+        return (
+            knex({
+                client: "pg",
+                connection: {
+                    connectionString: process.env.DATABASE_URL,
+                    ssl: true
+            }})
+        )
+    }
+}
+
+const postgresDB = DBEnvironment();
+
 const app = express();
-const fetch = require('node-fetch');
-
-
 
 app.get('/', (req, res) => res.send('money manager root get request'));
 
