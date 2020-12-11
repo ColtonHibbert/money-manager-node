71dc39437d974bcaae89099c738ace9f9629fde4
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-12-11 14:19:26 -0700

    docker compose and logging for security

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const bodyparser = require("body-parser");
 const cors = require("cors");
 const fetch = require("node-fetch");
 const knex = require("knex");
+const morgan = require("morgan");
 const config = require("./config/config.js");
-
 //won't break if .env is not present, won't overwrite default node_env or other env vars
 require("dotenv").config();
+
+
 
 function DBEnvironment() {
     if (process.env.NODE_ENV === "development") {
@@ -40,6 +42,8 @@ function DBEnvironment() {
 //const postgresDB = DBEnvironment();
 
 const app = express();
+
+app.use(morgan("combined"));
 
 app.get('/', (req, res) => res.send('money manager root get request'));
 
