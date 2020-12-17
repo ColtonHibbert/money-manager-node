96e81062ba1ef2cb7d63d66465adc8ee24872c70
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-12-17 14:40:44 -0700

    update to transaction constraints, fixed typo

--- postgres/constraints/transaction_constraints.sql
+++ postgres/constraints/transaction_constraints.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 
 ALTER TABLE transaction_ ADD constraint fk_category_id FOREIGN KEY(category_id) REFERENCES category(category_id);
-ALTER TABLE transaction_ ADD constraint fk_category_iitem_id FOREIGN KEY(category_item_id) REFERENCES category(category_item_id);
+ALTER TABLE transaction_ ADD constraint fk_category_item_id FOREIGN KEY(category_item_id) REFERENCES category_item(category_item_id);
 ALTER TABLE transaction_ ADD constraint fk_transaction_type_id FOREIGN KEY(transaction_type_id) REFERENCES transaction_type(transaction_type_id);
 ALTER TABLE transaction_ ADD constraint fk_user_id FOREIGN KEY(user_id) REFERENCES user_(user_id) ON DELETE CASCADE;
 ALTER TABLE transaction_ ADD constraint fk_account_id FOREIGN KEY(account_id) REFERENCES account(account_id) ON DELETE CASCADE;
