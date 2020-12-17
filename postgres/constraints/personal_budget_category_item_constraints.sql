c1d0f3a7057fd9d39f7263c686977a5e61534350
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-12-17 15:57:32 -0700

    continue seeding, add id to personal budget category items

--- postgres/constraints/personal_budget_category_item_constraints.sql
+++ postgres/constraints/personal_budget_category_item_constraints.sql
@@ -2,5 +2,6 @@ BEGIN TRANSACTION;
 
 ALTER TABLE personal_budget_category_item ADD constraint fk_personal_budget_category_id FOREIGN KEY(personal_budget_category_id) REFERENCES personal_budget_category(personal_budget_category_id) ON DELETE CASCADE;
 ALTER TABLE personal_budget_category_item ADD constraint fk_category_item_id FOREIGN KEY(category_item_id) REFERENCES category_item(category_item_id);
+ALTER TABLE personal_budget_category_item ADD constraint fk_user_id FOREIGN KEY(user_id) REFERENCES user_(user_id) ON DELETE CASCADE; 
 
 COMMIT;
