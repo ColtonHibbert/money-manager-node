36d22b2bbeeff88c3d81779a19e662f6004cfbd5
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-12-22 14:27:21 -0700

    login

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,21 @@
-const handleLogin = (req, res, next, postgresDB, bcrypt ) => {
+const handleLogin = async (req, res, next, postgresDB, bcrypt ) => {
     const { email, password, } =  req.body;
     if(!email || !password) {
-        return res.json("unable to login");
+        return res.status(400).json("unable to login");
     }
 
+    let hashedPassword;
+    try {
+        hashedPassword = bcrypt.hashSync(password, 14);
+    } catch(err) {
+        console.log("error with password on login");
+    }
+
+    const user = await postgresDB.select("*").from("user_")
+
+    const storedHashedPassword = await postgresDB.select("*").from
+
+    const verifyPassword = bcrypt.compareSync(hashedPassword, storedHashedPassword);
     
 
 }
