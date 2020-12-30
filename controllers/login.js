ca74db6087860f987e7a84094d1c26d41ceebfa6
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-12-30 15:58:45 -0700

    regenerate for

--- controllers/login.js
+++ controllers/login.js
@@ -17,34 +17,37 @@ const handleLogin = (async (req, res, next, postgresDB, bcrypt ) => {
     const verifyPassword = await bcrypt.compareSync(password, storedHashedPassword);
     
     if(verifyPassword) {
-        console.log(req.session);
-        console.log(req.session.id);
-        await postgresDB.transaction( (trx) => {
-            trx.insert({ 
-                session: req.session.id,
-                user_id: user.user_id
+        console.log(req.session.id, '1st session id log');
+        req.session.regenerate(async function (err) {
+            console.log(req.session.id, "session id log inside regen");
+            //reset session, prevent session fixation
+            await postgresDB.transaction( (trx) => {
+                trx.insert({ 
+                    session: req.session.id,
+                    user_id: user.user_id
+                })
+                .into("session")
+                .then(trx.commit)
+                .catch(trx.rollback)
             })
-            .into("session")
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch( (err) => {
-            res.status(400).json("unable to log in, server error");
-        })
-
-        req.session.userId = user.user_id;
-        req.session.firstName = user.first_name;
-        req.session.lastName = user.last_name;
-        req.session.email = user.email;
-        req.session.address = user.address;
-        req.session.phone = user.phone;
-        req.session.about = user.about;
-        req.session.joined = user.joined;
-        req.session.householdMemberId = user.household_member_id;
-        req.session.householdId = user.household_id;
-        req.session.roleId = user.role_id;
-
-        return res.send(JSON.stringify(user));
+            .catch( (err) => {
+                res.status(400).json("unable to log in, server error");
+            })
+    
+            req.session.userId = user.user_id;
+            req.session.firstName = user.first_name;
+            req.session.lastName = user.last_name;
+            req.session.email = user.email;
+            req.session.address = user.address;
+            req.session.phone = user.phone;
+            req.session.about = user.about;
+            req.session.joined = user.joined;
+            req.session.householdMemberId = user.household_member_id;
+            req.session.householdId = user.household_id;
+            req.session.roleId = user.role_id;
+    
+            return res.send(JSON.stringify(user));
+        });
     } 
     if(!verifyPassword) {
         res.status(400).json("unable to login");
