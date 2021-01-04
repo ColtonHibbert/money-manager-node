2e62e92eddd4225bdd59ec38b3b7b90e987439d6
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-01-04 11:30:52 -0700

    working on cookies local

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,6 @@
 
 const handleLogin = (async (req, res, next, postgresDB, bcrypt ) => {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+    //res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
 
 
     const { email, password } =  req.body;
