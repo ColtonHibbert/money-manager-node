1b77840d70765d799991231b5906e79102ad3e2f
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-01-08 15:42:46 -0700

    restructure data

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -8,7 +8,22 @@ const handleTransactions = (async (req, res, next, postgresDB ) => {
     const transactions = await postgresDB.select("*")
     .from("transaction_")
     .where("user_id", "=", userId)
-    .then(data => res.send(JSON.stringify(data)))
+    .then(data => {
+        let transactionResponseArray = [];
+
+        data.forEach(transaction => {
+            let transactionResponse = {
+                id: transaction.transaction_id,
+                amount: transaction.amount,
+                date: transaction.date,
+                memoNote: transaction.memo_note,
+                userId: transaction.user_id,
+                accountTypeId: transaction.account_type_id
+            }
+            transactionResponseArray.push(transactionResponse);
+        })
+        res.send(JSON.stringify(transactionResponseArray));
+    })
     .catch(err => console.log("error getting transactions"))
 })
 
