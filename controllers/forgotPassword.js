d878a5587bf28746d2c649c4150a722e2f620c0b
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-01-21 13:00:29 -0700

    forgot password route

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -7,17 +7,24 @@ const handleForgotPassword = (async (req, res, next, postgresDB ) => {
         return res.status(400).json({error: "Please enter an email"});
     }
 
-    const validEmail = await postgresDB.select("email").from("user_").where("email", "=", email)
+    const DBEmail = await postgresDB.select("email").from("user_").where("email", "=", email)
     .then(data => {
+        console.log("handleForgotPassword, db email data", data);
         if(data[0]) {
             return data[0]
         }
     })
     .catch(err => {
-        return res.status(400).json({error: "This email is not found."})
+        return res.status(400).json({error: "This email is not found. Please enter a valid email."})
     })
-    if(validEmail === undefined) {
-        return res.status(400).json({error: "This email is not found."})
+    if(DBEmail === undefined) {
+        return res.status(400).json({error: "This email is not found. Please enter a valid email."})
+    }
+
+    if(DBEmail !== undefined) {
+        const token = crypto.randomBytes(20).toString("hex");
+        
+
     }
 
 })
