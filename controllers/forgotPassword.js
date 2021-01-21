4135d65b6239a6aec2a6cbc6e08913ee9691400b
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-01-21 13:57:18 -0700

    password reset

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -39,7 +39,36 @@ const handleForgotPassword = (async (req, res, next, postgresDB, nodemailer ) =>
             return res.status(400).json({error: "Server error. Unable to send reset link."})
         })
 
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: `${process.env.RESET_EMAIL}`,
+                pass: `${process.env.RESET_PASSWORD}`
+            }
+        });
 
+        const mailOptions = {
+            from: "moneymanagerdemoreset@gmail.com",
+            to: `${DBUser.email}`,
+            subject: "Money Manager Password Reset Link",
+            text: 
+            "You are receiving this because you (or someone else) have requested the reset of the password on your account.\n\n" 
+            + "Please click on the link below or paste it the url bar in your browser and complete the password reset process.\n\n"
+            + "This link will expire in 15 minutes."
+            + `http://localhost:3000/passwordreset/${token}` + "\n\n"
+            + "If you do not wish to change your password then do not click on the link and your password will remain unchanged."
+        }
+
+        console.log("handleForgotPassword, after send password");
+
+        transporter.sendMail(mailOptions, (err, response) => {
+            if(err) {
+                console.log("There was an error sending email in forgotPasswordHandler.");
+                return res.status(400).json({ error: "There was an error sending to the specified email address."})
+            } else {
+                return res.status(200).json({emailSent: "success"})
+            }
+        })
 
     }
 
