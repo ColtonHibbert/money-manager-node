103f8c73ff30e682f516d970d8dbc2c95222a5df
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-01-22 11:18:48 -0700

    password reset token

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -57,7 +57,7 @@ const handleForgotPassword = (async (req, res, next, postgresDB, nodemailer, cry
             "You are receiving this because you (or someone else) have requested the reset of the password on your account.\n\n" 
             + "Please click on the link below or paste it the url bar in your browser and complete the password reset process.\n\n"
             + "This link will expire in 15 minutes."
-            + `http://localhost:3001/passwordreset/${token}` + "\n\n"
+            + `http://localhost:3000/?token=${token}` + "\n\n"
             + "If you do not wish to change your password then do not click on the link and your password will remain unchanged."
         }
 
