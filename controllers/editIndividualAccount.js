f95e666e709fc94469fe8c2209c7f210827177f7
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-02-05 15:59:01 -0700

    individual account

---
+++ controllers/editIndividualAccount.js
@@ -0,0 +1,8 @@
+const handleEditIndividualAccount = (req, res, next, postgresDB ) => {
+    // post
+    //account name, account type? Determine if this will cause issues and probably avoid, low alert balance, and delete
+}
+
+module.exports = {
+    handleEditIndividualAccount
+}
