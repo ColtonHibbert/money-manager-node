ef5f5336163363cb1e688af7c4cb5d442a0f68a6
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-02-08 12:27:24 -0700

    alter constraints for transaction, and categories, items

--- postgres/constraints/household_budget_category_item_constraints.sql
+++ postgres/constraints/household_budget_category_item_constraints.sql
@@ -2,5 +2,6 @@ BEGIN TRANSACTION;
 
 ALTER TABLE household_budget_category_item ADD constraint fk_household_budget_category_id FOREIGN KEY(household_budget_category_id) REFERENCES household_budget_category(household_budget_category_id) ON DELETE CASCADE;
 ALTER TABLE household_budget_category_item ADD constraint fk_category_item_id FOREIGN KEY (category_item_id) REFERENCES category_item(category_item_id);
+ALTER TABLE household_budget_category_item ADD constraint unique_household_category_item_per_household_category EXCLUDE USING gist(household_budget_category_id WITH =, category_item_id WITH =);
 
 COMMIT;
