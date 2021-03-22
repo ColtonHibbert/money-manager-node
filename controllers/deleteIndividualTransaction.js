21777bc6a4d3435444a1fe5dc0e59bbc6662d557
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-03-22 07:03:39 -0600

    delete transaction

--- controllers/deleteIndividualTransaction.js
+++ controllers/deleteIndividualTransaction.js
@@ -1,32 +1,71 @@
 //return accountid and transaction id to front end
 const handleDeleteIndividualTransaction = (async (req, res, next, postgresDB ) => {
+
     const {
-        transactionId
-    } = req.body;
+        deleteTransactionId
+        } = req.body;
 
-    let deletedUser = await postgresDB.transaction(trx => {
-        return trx("transaction_")
-        .where("transaction_id", "=", transactionId)
-        .returning(["transaction_id", "account_id"])
-        .del()
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => {
-        console.log("There was an error deleting the transaction.");
-    })
-    if(deletedUser === undefined) {
-        return res.status(400).json({error: "There was an error deleting the transaction."});
-    }
-    deletedUser = deletedUser[0];
-
-    const deletedUserResponse = {
-        transactionId: deletedUser.transaction_id,
-        accountId: deletedUser.account_id
-    }
-
-    return res.send(JSON.stringify(deletedUserResponse));
-
+        let deletedTransaction = await postgresDB.transaction(trx => {
+            return trx("transaction_")
+            .where("transaction_id", "=", deleteTransactionId)
+            .returning("*")
+            .del()
+            .then(trx.commit)
+            .catch(trx.rollback)
+        })
+        if(deletedTransaction === undefined) {
+            return res.status(400).json({error: "There was an error deleting the transaction"});
+        }
+        deletedTransaction = deltedTransaction[0]; 
+    
+        const configureAmount = () => {
+            if(deletedTransaction.transaction_type_id === 1 || deletedTransaction.transaction_type_id === 3) {
+                return Number(deletedTransaction.amount);
+            } else {
+                return -Number(deletedTransaction.amount);
+            }
+        }
+    
+        const configuredAmount = configureAmount();
+    
+        let account = await postgresDB.select("*").from("account").where("account_id", "=", deletedTransaction.account_id);
+        if(account === undefined) {
+            return res.status(400).json({error: "There was an error in deleting the transaction"});
+        }
+        account = account[0];
+    
+        let updatedAccount = await postgresDB.transaction(trx => {
+            return trx("account").where("account_id", "=", account.account_id)
+            .returning(["account_id", "current_balance"])
+            .update({
+                current_balance: Number(account.current_balance) + configuredAmount
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+        })
+        if(updatedAccount ===  undefined) {
+            return res.status(400).json({error: "There was an error updating the account while deleting this transaction."})
+        }
+        updatedAccount = updatedAccount[0];
+        console.log(updatedAccount);
+    
+        
+        const configuredTransaction = {
+            transactionId: deletedTransaction.transaction_id,
+        };
+    
+        const configuredAccount = {
+            accountId: updatedAccount.account_id,
+            currentBalance: Number(updatedAccount.current_balance)
+        }
+    
+        const transactionResponse = {
+            configuredTransaction,
+            configuredAccount
+        }
+    
+    
+       res.send(JSON.stringify(transactionResponse));
 })
 
 module.exports = {
